70661756816db0f49f6ae8e09067dbb7c8b7f784
Dimitris Tetepoulidis <48908714+jimtete@users.noreply.github.com>
2021-07-28 16:46:40 +0300

    it's functional

--- PasswordGeneratorSuper/generator/views.py
+++ PasswordGeneratorSuper/generator/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
+
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'generator/home.html',
-    {'password':'gamer'})
+    return render(request, 'generator/home.html')
 
-def eggs(request):
-    return HttpResponse("Eggs are awesome!")
+def password(request):
+
+    letters = list('abcdefghijklmnopqrstuvwxyz')
+
+    if (request.GET.get('uppercase')):
+        letters.extend("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    if (request.GET.get('special')):
+        letters.extend("$@!)(*&^%#_-+=?")
+    if (request.GET.get('numbers')):
+        letters.extend("0123456789")
+
+    length=int (request.GET.get('length', 12))
+
+    
+
+    thePassword=''
+    for i in range(length):
+        thePassword += random.choice(letters)
+
+
+    return render(request, 'generator/password.html', {'password':thePassword})
